70b99cae8a265914ee9e448e029ab74d350bdf2e
Beno Dwianto <benodwianto9@gmail.com>
2024-05-28 19:43:25 +0700

    initialize

---
+++ index.php
@@ -0,0 +1,486 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Pernikahan &mdash; Friska & Fauzan</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Pernikahan Friska dan Fauzan" />
+	<meta name="author" content="Beno Dww" />
+
+
+	<meta property="og:title" content="Pernikahan Friska dan Fauzan" />
+	<meta property="og:image" content="images/nikah.png" />
+	<meta property="og:url" content="" />
+	<meta property="og:site_name" content="Pernikahan Friska dan Fauzan" />
+	<meta property="og:description" content="Pernikahan Friska dan Fauzan" />
+	<meta name="twitter:title" content="Pernikahan Friska dan Fauzan" />
+	<meta name="twitter:image" content="images/nikah.png" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+	<link rel="icon" type="image/png" href="images/diamond.png">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="js/leaflet/leaflet.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+
+	<div class="fh5co-loader"></div>
+	<style type="text/css">
+		nav.fh5co-nav {
+			position: fixed;
+			/* Pastikan navbar tetap di atas saat di-scroll */
+			width: 100%;
+			/* Navbar akan memenuhi lebar layar */
+			z-index: 1000;
+			/* Pastikan navbar selalu di atas konten lain */
+			top: 0;
+		}
+	</style>
+	<div id="page">
+		<!-- Popup Modal -->
+		<div id="welcomeModal" class="modal" tabindex="-1" role="dialog">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title">Selamat Datang</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<p>Selamat datang di website undangan pernikahan kami! Kami berharap Anda menikmati kunjungan Anda.</p>
+						<form>
+							<div class="form-group">
+								<label for="namaPengunjung">Masukkan Nama Anda:</label>
+								<input type="text" class="form-control" id="namaPengunjung" placeholder="Nama Anda">
+							</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-primary" data-dismiss="modal">Oke</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End of Popup Modal -->
+		<nav class="fh5co-nav" role="navigation">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2">
+						<div id="fh5co-logo"><a href="index.html" style="color: #F14E95;">F&F<strong>.</strong></a></div>
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="#beranda">Beranda</a></li>
+							<li><a href="#acara">Acara</a></li>
+							<li><a href="#galeri">Galeri</a></li>
+							<li><a href="#map">Denah</a></li>
+							<li><a href="#kado">Kado</a></li>
+							<li><a href="#keutamaan">Keutamaan</a></li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</nav>
+
+		<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/img_bg_2.jpg); background-size: cover;" data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container" id="beranda">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center">
+						<div class="display-t">
+							<div class="display-tc animate-box" data-animate-effect="fadeIn">
+								<h2 id="namaTamu">Selamat Datang, Tamu!</h2>
+								<h1>Friska &amp; Fauzan</h1>
+								<h2>Kami Akan Melangsungkan Resepsi Pernikahan</h2>
+								<div class="simply-countdown simply-countdown-one"></div>
+								<p><a href="#" class="btn btn-default btn-sm">Mohon Doa Restu</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<div id="fh5co-couple">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+						<div class="col-md-12 text-center">
+							<img src="images/couple.png" alt="Foto Kecil" class="img-responsive center-block" style="max-width: 20%;">
+						</div>
+						<h2>Assalamualaikum!</h2>
+						<h4><i>Maha Suci Allah SWT yang Telah Menciptakan Makhluknya Berpasang - Pasangan, Ya Allah.... Untuk Mengikuti Sunnah Rasulmu Membentuk Keluarga Sakinah, Mawaddah & Warahmah, Kami Bermaksud Menyelenggarakan Akad & Resepsi Pernikahan Putra & Putri Kami:</i></h4>
+						<h3>Kamis, 27 Juni 2024 Batang Kapas, Pesisir Selatan</h3>
+						<p>Resepsi Pernikahan Kami</p>
+					</div>
+				</div>
+				<div class="couple-wrap animate-box">
+					<div class="couple-half">
+						<div class="groom">
+							<img src="images/1.png" alt="groom" class="img-responsive">
+						</div>
+						<div class="desc-groom">
+							<h3>Friska Wahyuni Hasri.S.Sos</h3>
+							<p>Putri pertama dari bapak Hasrizal Bakti.Sh & Ibu Sridarma Sustuti</p>
+						</div>
+					</div>
+					<p class="heart text-center"><i class="icon-heart2"></i></p>
+					<div class="couple-half">
+						<div class="bride">
+							<img src="images/2.png" alt="groom" class="img-responsive">
+						</div>
+						<div class="desc-bride">
+							<h3>Fauzan Azmi</h3>
+							<p>Putra dari Bapak Afridal & Ibu Asmaneli</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-event" class="fh5co-bg" style="background-image:url(images/img_bg_9.jpg);">
+			<div class="overlay"></div>
+			<div class="container" id="acara">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+						<span>Acara Spesial Kami</span>
+						<h2>Acara Pernikahan</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="display-t">
+						<div class="display-tc">
+							<div class="col-md-10 col-md-offset-1">
+								<div class="col-md-6 col-sm-6 text-center">
+									<div class="event-wrap animate-box">
+										<h3>AKAD NIKAH</h3>
+										<div class="event-col">
+											<i class="icon-clock"></i>
+											<span>10:00 Pagi</span>
+											<span>Selesai</span>
+										</div>
+										<div class="event-col">
+											<i class="icon-calendar"></i>
+											<span>Kamis 7</span>
+											<span>Maret, 2024</span>
+										</div>
+										<p>Kantor KUA Batang Kapas, Pasar Kuok, Kec Batang Kapas, Kab Pesisir Selatan</p>
+									</div>
+								</div>
+								<div class="col-md-6 col-sm-6 text-center">
+									<div class="event-wrap animate-box">
+										<h3>RESEPSI</h3>
+										<div class="event-col">
+											<i class="icon-clock"></i>
+											<span>09:00 pagi</span>
+											<span>Selesai</span>
+										</div>
+										<div class="event-col">
+											<i class="icon-calendar"></i>
+											<span>Kamis, 27</span>
+											<span>Juni, 2024</span>
+										</div>
+										<p>Kediaman Mempelai Wanita Limau Sundai IV, K Hilir, Kecamatan Batang Kapas, Kab Pesisir Selatan</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container mt-4">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box" style="margin-top: 50px;">
+					<div class="col-md-12 text-center">
+						<img src="images/notebook.png" alt="Foto Kecil" class="img-responsive center-block" style="max-width: 10%;">
+					</div>
+					<h2 id="galeri">Galeri Foto</h2>
+					<p><i>Photo-photo kebahagian kami yang akan kami kenang selalu.</i></p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 animate-box">
+					<img src="images/1weding.jpg" class="img-responsive img-hover-zoom" alt="Foto Pernikahan 1" style="transition: transform 0.5s ease, filter 0.5s ease; filter: brightness(100%);" onmouseover="this.style.filter='brightness(120%)'" onmouseout="this.style.filter='brightness(100%)'">
+					<p class="caption mb-3"></p>
+				</div>
+				<div class="col-md-4 animate-box">
+					<img src="images/2wed.jpg" class="img-responsive img-hover-zoom" alt="Foto Pernikahan 2" style="transition: transform 0.5s ease, filter 0.5s ease; filter: brightness(100%);" onmouseover="this.style.filter='brightness(120%)'" onmouseout="this.style.filter='brightness(100%)'">
+					<p class="caption"></p>
+				</div>
+				<div class="col-md-4 animate-box">
+					<img src="images/3wed.jpg" class="img-responsive img-hover-zoom" alt="Foto Pernikahan 3" style="transition: transform 0.5s ease, filter 0.5s ease; filter: brightness(100%);" onmouseover="this.style.filter='brightness(120%)'" onmouseout="this.style.filter='brightness(100%)'">
+					<p class="caption"></p>
+				</div>
+				<div class="col-md-6 animate-box">
+					<img src="images/4wed.jpg" class="img-responsive img-hover-zoom" alt="Foto Pernikahan 4" style="transition: transform 0.5s ease, filter 0.5s ease; filter: brightness(100%);" onmouseover="this.style.filter='brightness(120%)'" onmouseout="this.style.filter='brightness(100%)'">
+					<p class="caption"></p>
+				</div>
+				<div class="col-md-6 animate-box">
+					<img src="images/wed11.jpg" class="img-responsive img-hover-zoom" alt="Foto Pernikahan 5" style="transition: transform 0.5s ease, filter 0.5s ease; filter: brightness(100%);" onmouseover="this.style.filter='brightness(120%)'" onmouseout="this.style.filter='brightness(100%)'">
+					<p class="caption"></p>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-couple-story">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<img src="images/location.png" alt="Foto Kecil" class="img-responsive center-block" style="max-width: 10%;">
+					</div>
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+						<h2>Denah Resepsi</h2>
+					</div>
+				</div>
+				<div class="row">
+					<p class="text-center"><i>Anda dapat menuju lokasi resepsi kami dengan bantuan peta dibawah ini. Atau anda bisa buka di Google Maps</i></p>
+					<div id='maps-view-absen' style='width: 100%; height:500px;'></div>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-gifts" class="fh5co-bg" style="background-image:url(images/gift_bg.jpg);">
+			<div class="container">
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+						<div class="col-md-12 text-center">
+							<img src="images/love-letter.png" alt="Foto Kecil" class="img-responsive center-block" style="max-width: 10%;">
+						</div>
+						<h2 id="kado">Kado Pernikahan</h2>
+						<p><i>Terima kasih atas doa dan restu yang telah anda berikan, jika anda ingin mengirimkan kado nikah, silahkan kirim dengan cara dibawah ini. Sebelumnya, kami mengucapkan banyak terima kasih.</i></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 animate-box" style="padding-bottom: 20px;">
+						<img src="images/nagari-removebg-preview.png" alt="Kado Pernikahan" class="img-responsive center-block" style="margin-top: 20px; margin-bottom: 20px;transition: transform 0.5s ease, filter 0.5s ease; filter: brightness(100%);" onmouseover="this.style.filter='brightness(120%)'" onmouseout="this.style.filter='brightness(100%)'">
+						<h3 class="text-center">No. Rekening:</h3>
+						<h3 class="text-center mt-3" id="gift-info">04000210245835</h3>
+						<h3 class="text-center">Bank : Nagari</h3>
+						<h3 class="text-center">a.n. Friska Wahyuni Hasri</h3>
+						<div class="text-center">
+							<button class="btn btn-primary" onclick="copyToClipboard('#gift-info')"><img src="images/copy.png" class="img-responsive center-block" style="max-width: 5%;display:inline-block;" alt=""> Salin Informasi</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script>
+
+		</script>
+
+		<div id="fh5co-testimonial">
+			<div class="container" id="keutamaan">
+				<div class="row">
+					<div class="row animate-box">
+						<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+							<span>Dalil Pernikahan</span>
+							<h2>Keutamaan Menikah</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 animate-box">
+							<div class="wrap-testimony">
+								<div class="owl-carousel-fullwidth">
+									<div class="item">
+										<div class="testimony-slide active text-center">
+											<figure>
+												<img src="images/nikah.png" alt="user">
+											</figure>
+											<span>Allah Berfirman:</span>
+											<blockquote>
+												<p>“Dan segala sesuatu Kami Ciptakan Berpasang-pasangan supaya kamu mengingat kebesaran Allah.”(QS Az-Zariyat: 49)</p>
+											</blockquote>
+										</div>
+									</div>
+
+									<div class="item">
+										<div class="testimony-slide active text-center">
+											<figure>
+												<img src="images/nikah.png" alt="user">
+											</figure>
+											<span>Allah Berfirman:</span>
+											<blockquote>
+												<p>“Bagaimana kamu akan mengambilnya kembali, padahal sebagian kamu telah bergaul (bercampur) dengan yang lain sebagai suami-isteri. Dan mereka (isteri-isterimu) telah mengambil dari kamu perjanjian yang kuat.” (QS An-Nisa: 21)</p>
+											</blockquote>
+										</div>
+									</div>
+									<div class="item">
+										<div class="testimony-slide active text-center">
+											<figure>
+												<img src="images/nikah.png" alt="user">
+											</figure>
+											<span>Rasulullah SAW bersabda:</span>
+											<blockquote>
+												<p>“Siapa yang ingin bertemu Allah dalam keadaan suci dan disucikan, maka menikahlah dengan perempuan-perempuan merdeka.” (HR Ibnu Majah)</p>
+											</blockquote>
+										</div>
+									</div>
+									<div class="item">
+										<div class="testimony-slide active text-center">
+											<figure>
+												<img src="images/nikah.png" alt="user">
+											</figure>
+											<span>Rasulullah SAW bersabda:</span>
+											<blockquote>
+												<p> “Siapa yang menikah maka sungguh ia telah diberi setengahnya ibadah.” (HR Abu Ya’la)</p>
+											</blockquote>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<img src="images/img_bg_2.jpg" alt="" style="width:100%; height:200px; object-fit:cover;">
+		<div class="row">
+			<div class="col-md-12">
+				<img src="images/divider.png" alt="" style="width:100%; height:auto; object-fit:cover;">
+			</div>
+		</div>
+
+		<footer id="fh5co-footer" role="contentinfo">
+			<div class="container">
+
+				<div class="row copyright">
+					<div class="col-md-12 text-center">
+						<p>
+							<small class="block">&copy; 2024 <a href="djazuli.my.id">Beno Dwianto</a>.</small>
+						</p>
+					</div>
+				</div>
+
+			</div>
+		</footer>
+	</div>
+
+
+	<audio autoplay="true" loop="true" controls>
+		<source src="audio.mp3" type="audio/mpeg">
+	</audio>
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+
+	<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
+	<script src="js/simplyCountdown.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+	<script src="js/leaflet/leaflet.js"></script>
+	<script>
+		if (document.getElementById("maps-view-absen")) {
+			var map = L.map('maps-view-absen').setView([-1.4538381, 100.5998501], 15);
+
+			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Beno Dwianto'
+			}).addTo(map);
+
+			L.marker([-1.4538381, 100.5998501]).addTo(map);
+		}
+
+		function copyToClipboard(element) {
+			var $temp = $("<input>");
+			$("body").append($temp);
+			$temp.val($(element).text()).select();
+			document.execCommand("copy");
+			$temp.remove();
+			alert("Informasi telah disalin ke clipboard");
+		}
+	</script>
+	<script>
+		// Waktu sekarang
+		var now = new Date();
+
+		// Tambahkan selisih milidetik ke waktu sekarang
+		var targetTime = new Date(now.getTime() + 3013200000);
+
+		// Konfigurasi simplyCountdown untuk '.simply-countdown-one'
+		simplyCountdown('.simply-countdown-one', {
+			year: targetTime.getFullYear(),
+			month: targetTime.getMonth() + 1,
+			day: targetTime.getDate(),
+			hours: targetTime.getHours(),
+			minutes: targetTime.getMinutes(),
+			seconds: targetTime.getSeconds(),
+			enableUtc: false
+		});
+
+		// Konfigurasi simplyCountdown untuk '#simply-countdown-losange'
+		$('#simply-countdown-losange').simplyCountdown({
+			year: targetTime.getFullYear(),
+			month: targetTime.getMonth() + 1,
+			day: targetTime.getDate(),
+			hours: targetTime.getHours(),
+			minutes: targetTime.getMinutes(),
+			seconds: targetTime.getSeconds(),
+			enableUtc: false
+		});
+
+		$(document).ready(function() {
+			$('#welcomeModal').modal('show');
+		});
+
+		document.getElementById('namaPengunjung').addEventListener('input', function() {
+			document.getElementById('namaTamu').textContent = 'Selamat Datang, ' + this.value + '!';
+		});
+	</script>
+
+</body>
+
+</html>
